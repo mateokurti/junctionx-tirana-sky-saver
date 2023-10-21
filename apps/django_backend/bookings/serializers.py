448bcf675ab12c257fdc1cf36b1d576c14103635
Mateo Kurti <contact@mateokurti.com>
2023-10-21 23:43:29 +0200

    Add total_price to booking and ticket serializers

--- apps/django_backend/bookings/serializers.py
+++ apps/django_backend/bookings/serializers.py
@@ -13,16 +13,24 @@ class BookedSegmentsSerializer(serializers.ModelSerializer):
 class TicketSerializer(serializers.ModelSerializer):
     traveller = TravellerSerializer(read_only=True)
     booked_segments = BookedSegmentsSerializer(many=True, read_only=True, source="bookedsegment_set")
+    total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Ticket
         fields = "__all__"
 
+    def get_total_price(self, obj) -> float:
+        return sum(segment.price for segment in obj.bookedsegment_set.all())
+
 
 class BookingSerializer(serializers.ModelSerializer):
     user = UserAccountSerializer(read_only=True)
     tickets = TicketSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Booking
         fields = "__all__"
+
+    def get_total_price(self, obj) -> float:
+        return sum(ticket.total_price for ticket in obj.tickets.all())
